4b5cd9081f147601a7993402115cadf548464ccc
GouveaHeitor <hi@heitorgouvea.me>
2016-12-28 17:28:33 -0200

    added new modules at Notify

---
+++ lib/Horus/Framework/Modules/Notify/Slack.pm
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+#########################################################
+# Horus developed by Heitor Gouvêa                      #
+# This work is licensed under MIT License               #
+# Copyright (c) 2016 Heitor Gouvêa                      #
+#                                                       #
+# [+] AUTOR:        Heitor Gouvêa                       #
+# [+] EMAIL:        hi@heitorgouvea.me                  #
+# [+] GITHUB:       https://github.com/GouveaHeitor     #
+# [+] TWITTER:      https://twitter.com/GouveaHeitor    #
+# [+] FACEBOOK:     https://fb.com/GouveaHeitor         #
+#########################################################
+
+
+package Horus::Framework::Modules::Notify::Slack;
+
+sub new {
+
+}
+
+1;
