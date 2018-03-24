5430b18c1f96cebfc3e15ec6147ab4909dd3c0a0
GouveaHeitor <hi@heitorgouvea.me>
2018-03-24 01:20:57 -0300

    develop web/adminfinder.pl

---
+++ web/adminfinder.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+use 5.010;
+use strict;
+use warnings;
+use LWP::UserAgent;
+
+sub main {
+  my $target = $ARGV[0];
+
+  if ($target) {
+    open (my $wordlist, "<", "wordlists/admin-pages.txt");
+
+    my $userAgent = LWP::UserAgent -> new();
+
+    $userAgent -> timeout(15);
+    $userAgent -> agent ("Mozilla/5.0 (Windows; U; Windows NT 5.1; en; rv:1.9.0.4) Gecko/2008102920 Firefox/3.0.4");
+
+    while (<$wordlist>) {
+      chomp ($_);
+
+      my $request = $userAgent -> get("$target/$_");
+      my $httpCode = $request -> code();
+
+      if ($httpCode == "200") {
+        print "$target/$_\n";
+      }
+    }
+
+    close ($wordlist);
+  }
+}
+
+main();
+exit;
