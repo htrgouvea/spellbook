1edea02870e0d1062b89592d04be001fe7d5e356
GouveaHeitor <hi@heitorgouvea.me>
2018-05-04 20:16:36 -0300

    update web/links.pl

---
+++ bruteforce/ftp.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+# Simple script to execute brute force attack on FTP services
+# Use: ./ftp.pl 192.168.0.1 root wordlist.txt
+# Heitor Gouvêa - hi@heitorgouvea.me
+
+use 5.010;
+use strict;
+use warnings;
+
+sub main {
+  my $target = $ARGV[0];
+
+  if ($target) {
+
+  }
+}
+
+main();
+exit;
--- web/links.pl
+++ web/links.pl
@@ -14,7 +14,50 @@ sub main {
   my $target = $ARGV[0];
 
   if ($target) {
+    if ($target !~ /^https?:/) {
+      $target = 'http://' . $target;
+    }
 
+    my @urls = $target;
+    my %seen = ();
+    my @links = ();
+
+    while (my $link = shift @urls) {
+      next if $seen{$link}++;
+
+      print $link . "\n";
+
+      my $host = URI -> new($link) -> host();
+
+      my $mech = WWW::Mechanize -> new();
+
+      eval {
+        $mech -> get($link);
+      };
+
+      return if ($@);
+
+      my @links = $mech -> links();
+      my %seen = ();
+
+      foreach my $uri (@links) {
+        my $url = $uri -> url_abs();
+        next if $url eq $link;
+        my $hurl;
+
+        eval { $hurl = URI -> new($url) -> host(); };
+
+        next unless $hurl;
+
+        if ($hurl =~ $host) {
+          next if $seen{$url}++;
+        }
+      }
+
+      @links = keys %seen;
+
+      push @urls, @links;
+    }
   }
 }
 
