db2472b6d90ccc8c09fe4f50ce101f64e8fdd1aa
GouveaHeitor <hi@heitorgouvea.me>
2019-08-25 15:41:27 -0300

    create http fuzzer

--- web/httpfuzzer.pl
+++ web/httpfuzzer.pl
@@ -19,22 +19,21 @@ sub main {
         ];
 
         my @verbs = (
-            '', 'GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS', 'CONNECT', 'TRACE', 'PATCH', '1337', '*',
-            '@GET', 'JEFF', 'CATS', '🥰😍', '😭😢', 
+            "GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "CONNECT", "TRACE", "PATCH", "SUBSCRIBE", "MOVE", "REPORT",
+            "UNLOCK", "0", "%s%s%s%s", "PURGE", "POLL", "OPTIONS", "NOTIFY", "SEARCH", "1337", "JEFF", "CATS", "*"
         );
         
         open (my $routes, "<", $wordlist);
 
         while (<$routes>) {
             chomp ($_);
-
+            
             my $endpoint = $target . $_;
 
             foreach my $verb (@verbs) {
-                my $request  = new HTTP::Request($verb, $endpoint, $header);
-                my $response = $userAgent -> request($request);
-
-                my $httpCode = $response -> code;
+                my $request     = new HTTP::Request($verb, $endpoint, $header);
+                my $response    = $userAgent -> request($request);
+                my $httpCode    = $response -> code;
                 my $httpMessage = $response -> message();
 
                 print "[ ! ] -> [$httpCode] | $endpoint \t [$verb] - $httpMessage\n";
