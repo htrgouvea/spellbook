3ec32f8c7b9274ebd7c81482f95e6fb7dce56403
GouveaHeitor <hi@heitorgouvea.me>
2019-10-04 12:15:02 -0300

    push a draft scripts

---
+++ misc/apk_sing.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+def main
+    apkfile  = ARGV[0]
+    pkgname  = ARGV[1]
+    password = ARGV[2]
+
+    if apkfile && pkgname && password
+        # system('keytool -genkey -keystore', pkgname, '.jks -storepass', password, '-storetype jks -alias $nome -keyalg rsa -dname "CN=DESEC" -keypass', password)
+        # system('jarsigner -keystore', pkgname, '.jks -storepass', password, '-storetype jks -sigalg sha1withrsa -digestalg sha1', apkfile, pkgname)
+        # system('jarsigner -verify -certs -verbose', apkfile)
+    else 
+        puts "ruby apk_sing.rb <apkfile.apk> <package_name> <password>\n"
+    end
+end
+
+main
+exit
