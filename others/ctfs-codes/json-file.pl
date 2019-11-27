ca9c69200c99b10ff4e9725d807802cf8556449d
GouveaHeitor <hi@heitorgouvea.me>
2019-11-27 15:16:42 -0300

    update things

---
+++ others/ctfs-codes/json-file.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl
+
+use 5.010;
+use strict;
+use warnings;
+use JSON::MaybeXS qw(encode_json decode_json);
+
+sub main {
+    my $values = {
+
+    };
+
+}
+
+main();
+exit;
--- web/extract_links.pl
+++ web/extract_links.pl
@@ -1,16 +1,27 @@
 #!/usr/bin/env perl
 
-# draft code
+# Usage: perl extract_links.pl https://target.com
 
 use 5.010;
 use strict;
 use warnings;
+use WWW::Mechanize;
 
 sub main {
     my $target = $ARGV[0];
 
     if ($target) {
+        my $mech    = WWW::Mechanize -> new();
+        my $request = $mech -> get($target);
+        my $status  = $mech -> status();
 
+        if ($status == "200") {
+            my @links = $mech -> links();
+
+            for my $link (@links) {
+                print $link -> url(), "\n";
+            }
+        }
     }
 }
 
