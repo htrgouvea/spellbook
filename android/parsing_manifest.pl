6e7196249b2243a2d27a8884052a90d6d75554c1
GouveaHeitor <hi@heitorgouvea.me>
2020-02-07 17:13:41 -0300

    wel

--- android/parsing_manifest.pl
+++ android/parsing_manifest.pl
@@ -5,12 +5,22 @@
 use 5.018;
 use strict;
 use warnings;
+use XML::Simple;
 
 sub main {
-    my $target = $ARGV[0];
+    my $file = $ARGV[0];
 
-    if ($target) {
-        print "here\n";
+    if ($file) {
+        my $data = XMLin($file);
+
+        my $package = $data -> {package};
+        my $backup = $data -> {application} -> {'android:allowBackup'};
+        my $debug = $data -> {application} -> {'android:debuggable'};
+
+        print "
+            \r[ - ] | Package name: $package
+            \r[ - ] | Debug -> $debug
+            \r[ - ] | Backup -> $backup\n";
     }
 }
 
