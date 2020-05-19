3a3a54dce1ad643f5cb60eb03d69c59409cfde04
GouveaHeitor <hi@heitorgouvea.me>
2020-05-19 20:05:48 -0300

    update labs

--- labs/ssrf/application.pl
+++ labs/ssrf/application.pl
@@ -12,7 +12,7 @@ use Mojolicious::Lite -signatures;
 get "/" => sub ($request) {
     my $read = $request -> param("read");
 
-    if ($read) { 
+    if (($read) && ($read <= 52)) { 
         my $url = Mojo::URL -> new($read);
 
         my $scheme = $url -> scheme || " ";
@@ -21,24 +21,24 @@ get "/" => sub ($request) {
         my $port = $url -> port || " ";
         my $path = $url -> path || " ";
 
-        say "scheme -> $scheme";
-        say "userinfo -> $userinfo";
-        say "host -> $host";
-        say "port -> $port";
-        say "path -> $path";
-        say "========================";
+        # say "scheme -> $scheme";
+        # say "userinfo -> $userinfo";
+        # say "host -> $host";
+        # say "port -> $port";
+        # say "path -> $path";
+        # say "========================";
 
         my $userAgent = Mojo::UserAgent -> new();
 
         my @blacklist = (
             "localhost", "0.0.0.0", "127.0.0.1", "127.0.0.2", "169.254.169.254",
-            "0:0:0:0:0:ffff:a9fe:a9fe", "::ffff:a9fe:a9fe", "[::]", "0000::1",
+            "0:0:0:0:0:ffff:a9fe:a9fe", "::ffff:a9fe:a9fe", "[::]", "0000::1"
         );
 
         foreach my $hit (@blacklist) {
             if ($host eq $hit) {
                 return ($request -> render ( 
-                    text => "Sorry bro! You can do better than that man!"
+                    text => "Sorry bro! You can do better!!!"
                 )); 
             }
         }
@@ -49,7 +49,17 @@ get "/" => sub ($request) {
             my $content = $getContent -> body();
 
             return ($request -> render ( 
-                text => $content
+                text => "<html>
+                    <head>
+                        <title>Awesome Cloud Security</title>
+                        <!-- The flag is internal hostname of this server ;) -->
+                    </head>
+                    <body>
+                        $content
+                    </body>
+                    
+                </html>"
+                
             ));   
         }
         
