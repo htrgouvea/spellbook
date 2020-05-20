65980a35f4362af294c1edbbbc1a692cad0ba4f6
GouveaHeitor <hi@heitorgouvea.me>
2020-05-19 21:24:48 -0300

    fix a error on calc length

--- labs/ssrf/application.pl
+++ labs/ssrf/application.pl
@@ -12,7 +12,7 @@ use Mojolicious::Lite -signatures;
 get "/" => sub ($request) {
     my $read = $request -> param("read");
 
-    if (($read) && ($read <= 52)) { 
+    if (($read) && (length($read) <= 52)) { 
         my $url = Mojo::URL -> new($read);
 
         my $scheme = $url -> scheme || " ";
@@ -21,18 +21,19 @@ get "/" => sub ($request) {
         my $port = $url -> port || " ";
         my $path = $url -> path || " ";
 
-        # say "scheme -> $scheme";
-        # say "userinfo -> $userinfo";
-        # say "host -> $host";
-        # say "port -> $port";
-        # say "path -> $path";
-        # say "========================";
+        say "scheme -> $scheme";
+        say "userinfo -> $userinfo";
+        say "host -> $host";
+        say "port -> $port";
+        say "path -> $path";
+        say "========================";
 
         my $userAgent = Mojo::UserAgent -> new();
 
         my @blacklist = (
             "localhost", "0.0.0.0", "127.0.0.1", "127.0.0.2", "169.254.169.254",
-            "0:0:0:0:0:ffff:a9fe:a9fe", "::ffff:a9fe:a9fe", "[::]", "0000::1"
+            "0:0:0:0:0:ffff:a9fe:a9fe", "::ffff:a9fe:a9fe", "[::]", "0000::1", "0xA9.0xFE.0xA9.0xFE",
+            
         );
 
         foreach my $hit (@blacklist) {
