71c189c24dda1ecae742b9df787551fbfbe81baf
htrgouvea <hi@heitorgouvea.me>
2021-02-03 15:23:42 -0300

    Update new_target.pm helper

--- lib/Spellbook/Auxiliary/New_Target.pm
+++ lib/Spellbook/Auxiliary/New_Target.pm
@@ -6,25 +6,14 @@ use warnings;
 sub new {
     my ($self, $target) = @_;
 
-    print $target;
-
     if ($target) {
         my @folders = ("recon", "notes", "files");
-        my @recon   = ("amass", "emails", "shodan", "zoomeye", "nmap",  "headers");
         my @files   = ("exploits", "proofs");
-        
-        print "here";
 
         my @result = (); 
 
-        foreach my $folder(@folders) {
-            if ($folder eq "recon") {
-                foreach my $recon (@recon) {
-                    system("mkdir -p $target/$folder/$recon");
-                }
-            }
-
-            elsif ($folder eq "files") {
+        foreach my $folder (@folders) {
+            if ($folder eq "files") {
                 foreach my $file (@files) {
                     system("mkdir -p $target/$folder/$file");
                 }
