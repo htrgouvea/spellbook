5b9711d6072c00ef560aed0c4f1fcadb5cd13a8c
htrgouvea <hi@heitorgouvea.me>
2021-03-13 13:58:31 -0300

    update @results array

--- lib/Spellbook/Recon/Shodan_Enum.pm
+++ lib/Spellbook/Recon/Shodan_Enum.pm
@@ -1,39 +1,37 @@
-package Spellbook::Recon::Shodan_Enum;
+package Spellbook::Recon::Shodan_Enum {
+    use strict;
+    use warnings;
+    use JSON;
+    use LWP::UserAgent;
+    use Spellbook::Core::Credentials;
 
-use strict;
-use warnings;
-use JSON;
-use LWP::UserAgent;
-use Spellbook::Core::Credentials;
-
-sub new {
-    my ($self, $ip) = @_;
-
-    if ($ip) {
-        my $apiKey    = Spellbook::Core::Credentials -> new("shodan");
-        my $endpoint  = "https://api.shodan.io/shodan/host/$ip?key=$apiKey";
-        my $userAgent = LWP::UserAgent -> new();
-	    my $request   = $userAgent -> get($endpoint);
-	    my $httpCode  = $request -> code();
-
+    sub new {
+        my ($self, $ip) = @_;
         my @results = ();
 
-        if ($httpCode == 200) {
-            my $content = decode_json($request -> content);
+        if ($ip) {
+            my $apiKey    = Spellbook::Core::Credentials -> new("shodan");
+            my $endpoint  = "https://api.shodan.io/shodan/host/$ip?key=$apiKey";
+            my $userAgent = LWP::UserAgent -> new();
+            my $request   = $userAgent -> get($endpoint);
+            my $httpCode  = $request -> code();
 
-            foreach my $data (@{$content -> {'data'}}) {
-                my $product   = $data -> {'product'} || "unknown";
-                my $port      = $data -> {'port'};
-                my $transport = $data -> {'transport'};
-                my $service   = $data -> {'_shodan'} -> {'module'};
+            if ($httpCode == 200) {
+                my $content = decode_json($request -> content);
 
-                push @results, "$ip | $transport | $port | $service | $product\n";
+                foreach my $data (@{$content -> {'data'}}) {
+                    my $product   = $data -> {'product'} || "unknown";
+                    my $port      = $data -> {'port'};
+                    my $transport = $data -> {'transport'};
+                    my $service   = $data -> {'_shodan'} -> {'module'};
+
+                    push @results, "$ip | $transport | $port | $service | $product\n";
+                }
             }
-        }
+        }   
 
         return @results;
-    }   
-
+    }
 }
 
 1;
