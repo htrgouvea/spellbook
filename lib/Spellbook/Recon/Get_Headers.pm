aab85657a0fd41d669d13a20375f33a87089202c
htrgouvea <hi@heitorgouvea.me>
2021-03-13 22:23:08 -0300

    comment ssl verify

--- lib/Spellbook/Recon/Get_Headers.pm
+++ lib/Spellbook/Recon/Get_Headers.pm
@@ -6,21 +6,20 @@ package Spellbook::Recon::Get_Headers {
 
     sub new {
         my ($self, $hostname) = @_;
-
         my @results = ();
 
         if ($hostname) {
             my $ua = LWP::UserAgent -> new();
+            # $ua -> ssl_opts(verify_hostname => 0);
 
             try {
-                my $request = $ua -> head("https://$hostname");
+                my $request = $ua -> head("http://$hostname");
                 push @results, $request -> headers_as_string, "\n";
             }
 
             catch {
                 #
             }
-            
         }
 
         return @results;
