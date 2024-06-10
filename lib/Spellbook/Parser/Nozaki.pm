0d72a6df9ba1ecd10b4de104ff84c46e5060f6cf
htrgouvea <hi@heitorgouvea.me>
2024-06-10 15:15:16 -0300

    draft module

---
+++ lib/Spellbook/Parser/Nozaki.pm
@@ -0,0 +1,37 @@
+package Spellbook::Parser::Nozaki {
+    use strict;
+    use warnings;
+    use JSON;
+    use Mojo::File;
+
+    sub new {
+        my ($self, $parameters) = @_;
+        my ($help, $file, @result);
+
+        Getopt::Long::GetOptionsFromArray (
+            $parameters,
+            "h|help"   => \$help,
+            "f|file=s" => \$file,
+        );
+
+        if ($file) {
+            my $load_file = Mojo::File -> new($file);
+            my $data     = $load_file -> slurp();
+            my $content  = decode_json($data);
+
+            print $content;
+        }
+
+        if ($help) {
+            return "
+                \rParser::Nozaki
+                \r=====================
+                \r-h, --help     See this menu
+                \r-t, --target   \n\n";
+        }
+
+        return 0;
+    }
+}
+
+1;
