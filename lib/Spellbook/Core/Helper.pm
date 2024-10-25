be1fa74084c7765505a04cb5e1de4c05922b221a
htrgouvea <hi@heitorgouvea.me>
2024-10-25 13:33:12 +0100

    update version

--- lib/Spellbook/Core/Helper.pm
+++ lib/Spellbook/Core/Helper.pm
@@ -4,7 +4,7 @@ package Spellbook::Core::Helper {
 
 	sub new {
 		print "
-            \rSpellbook v0.3.4
+            \rSpellbook v0.3.5
             \rCore Commands
             \r==============
             \r\tCommand          Description
