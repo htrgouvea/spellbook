98bb31fbc8b8b43036f79547d6b72cb0a771745c
htrgouvea <canton_ankh.0f@icloud.com>
2025-01-19 20:42:51 +0300

    new module to identify the account id from AWS key

---
+++ lib/Spellbook/Cloud/Account_Identifier.pm
@@ -0,0 +1,42 @@
+package Spellbook::Cloud::Account_Identifier {
+    use strict;
+    use warnings;
+    use MIME::Base32 qw(decode_base32);
+    use Math::BigInt;
+
+    sub new {
+        my ($self, $parameters) = @_;
+        my ($help, $key);
+
+        Getopt::Long::GetOptionsFromArray (
+            $parameters,
+            "h|help"  => \$help,
+            "k|key=s" => \$key
+        );
+            
+        if ($key) {
+            my $trimmed_AWSKeyID = substr($key, 4);
+            my $decoded          = decode_base32($trimmed_AWSKeyID);
+            
+            my $decoded_prefix = substr($decoded, 0, 6);
+            my $bigint_value = Math::BigInt -> new('0x' . unpack("H*", $decoded_prefix));
+            
+            my $mask      = Math::BigInt -> new('0x7fffffffff80');
+            my $accountID = ($bigint_value & $mask) >> 7;
+
+            return $accountID;
+        }
+
+        if ($help) {
+            return "
+                \rCloud::Account_Identifier
+                \r==============
+                \r-h, --help       See this menu
+                \r-k, --key        Provides the AWS key for extracting the account ID.\n\n";
+        }
+        
+        return 0;
+    }    
+}
+
+1;
