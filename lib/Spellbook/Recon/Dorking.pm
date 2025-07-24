a3ff66e78143345bea228660aa7fae722ea366e1
htrgouvea <canton_ankh.0f@icloud.com>
2025-07-23 23:10:57 -0300

    first version of Recon::Dorking module using Bing

--- lib/Spellbook/Recon/Dorking.pm
+++ lib/Spellbook/Recon/Dorking.pm
@@ -1,20 +1,47 @@
 package Spellbook::Recon::Dorking {
     use strict;
     use warnings;
-    use WWW::DuckDuckGo;
+    use WWW::Mechanize;
+    use Mojo::Util qw(url_escape);
 
     sub new {
         my ($self, $parameters)= @_;
         my ($help, $dork, @result);
+        
+        my $page = 10;
 
         Getopt::Long::GetOptionsFromArray (
             $parameters,
             "h|help"   => \$help,
-            "d|dork=s" => \$dork
+            "d|dork=s" => \$dork,
+            "p|page=i" => \$page
         );
 
         if ($dork) {
-            my $engine = WWW::DuckDuckGo -> new() -> zci("$dork");
+            $dork = url_escape($dork);
+
+            my %seen  = ();
+            my $mech = WWW::Mechanize -> new();
+
+            $mech -> ssl_opts (verify_hostname => 0);
+
+            for (0 .. $page) {
+                my $url = "https://wwww.bing.com/search?q=${dork}&first=${page}0";
+
+                $mech -> get($url);
+
+                my @links = $mech -> links();
+
+                foreach my $link (@links) {
+                    $url = $link -> url();
+
+                    next if $seen{$url}++;
+
+                    if ($url =~ m/^https?/x && $url !~ m/bing|live|microsoft|msn/x) {
+                        push @result, $url;
+                    }
+                }
+            }
 
             return @result;
         }
@@ -24,6 +51,9 @@ package Spellbook::Recon::Dorking {
                 \rRecon::Dorking
                 \r=====================
                 \r-h, --help     See this menu
+                \r-d, --dork     Set a dork to search
+                \r-p, --page     Set the number of pages to search (default: 10)
+                \r\n
                 \r \n\n";
         }
 
