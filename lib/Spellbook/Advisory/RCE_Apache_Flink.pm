b9fdf105c8ee12501418cf6299234aa20279c840
htrgouvea <hi@heitorgouvea.me>
2025-08-08 15:00:36 -0300

    draft

---
+++ lib/Spellbook/Advisory/RCE_Apache_Flink.pm
@@ -0,0 +1,14 @@
+package Spellbook::Advisory::RCE_Apache_Flink {
+    use strict;
+    use warnings;
+
+    sub new {
+        
+    }
+}
+
+1;
+# https://app.vidocsecurity.com/public-library/8f23c5a1-c60f-4ff7-b4b8-f4c514f04764
+# https://www.exploit-db.com/exploits/48978
+# Google Dorks: intitle:"Apache Flink Web Dashboard"
+# Shodan dorks: http.title:"Apache Flink Web Dashboard"
