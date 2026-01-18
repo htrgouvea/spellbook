28354b521535bd85e86350fb6ed5394b462031ad
htrgouvea <10741284+htrgouvea@users.noreply.github.com>
2026-01-18 13:42:14 -0300

    Share connection cache for user agents

--- lib/Spellbook/Core/UserAgent.pm
+++ lib/Spellbook/Core/UserAgent.pm
@@ -2,30 +2,26 @@ package Spellbook::Core::UserAgent {
     use strict;
     use warnings;
     use LWP::UserAgent;
+    use LWP::ConnCache;
 
     our $VERSION = '0.0.2';
 
-    our $sharedUserAgent;
+    our $sharedConnectionCache = LWP::ConnCache -> new();
 
     sub new {
-        if (defined $sharedUserAgent) {
-            return $sharedUserAgent;
-        }
-
         my $userAgent = LWP::UserAgent -> new (
             timeout  => 5,
             ssl_opts => {
                 verify_hostname => 0,
                 SSL_verify_mode => 0
             },
-            agent => 'Spellbook / v0.3.8'
+            agent => 'Spellbook / v0.3.8',
+            conn_cache => $sharedConnectionCache
         );
 
         $userAgent -> default_headers -> push_header("Cache-Control" => "no-cache");
 
-        $sharedUserAgent = $userAgent;
-
-        return $sharedUserAgent;
+        return $userAgent;
     }
 }
 
