8ce6c8052ae38982571e3ac5259da4c98403db44
htrgouvea <hi@heitorgouvea.me>
2026-04-02 21:43:38 -0300

    starting the versioning

--- lib/Spellbook/Advisory/CVE_2021_41174.pm
+++ lib/Spellbook/Advisory/CVE_2021_41174.pm
@@ -3,6 +3,8 @@ package Spellbook::Advisory::CVE_2021_41174 {
     use warnings;
     use Spellbook::Core::UserAgent;
 
+    our $VERSION = '0.0.1';
+
     sub new {
         my ($self, $parameters) = @_;
         my ($help, $target, $snapshot_path, @result);
--- lib/Spellbook/Advisory/RCE_Apache_Flink.pm
+++ lib/Spellbook/Advisory/RCE_Apache_Flink.pm
@@ -2,6 +2,8 @@ package Spellbook::Advisory::RCE_Apache_Flink {
     use strict;
     use warnings;
 
+    our $VERSION = '0.0.1';
+
     sub new {
 
     }
